b927920f87e57429490e92fe820ede25fac57851
matvey0479 <matvey0479@mail.ru>
2024-07-15 19:01:03 +0700

    add jqueri and ajax

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
+using Test.Domain;
+using Test.Domain.Entites;
 using Test.Models;
 
 namespace Test.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly PriceContext context;
+        public HomeController(PriceContext context) 
+        {
+            this.context = context;
+        } 
        
         public IActionResult Index()
         {
@@ -13,7 +21,29 @@ namespace Test.Controllers
         }
         public IActionResult AddingPriceList() 
         {
-            return View();
+            return View(new Column());
+        }
+        [HttpPost]
+        public JsonResult AddPriceList(Column column/*,PriceList priceList*/)
+        {
+            //List<Column> columns= new List<Column>();
+            //for (int i=0;i<=Request.Form.Count;i++) 
+            //{
+            //    var Name = Request.Form["Name[" + i + "]"];
+            //    var DataType = Request.Form["DataType[" + i + "]"];
+            //    if (!String.IsNullOrEmpty(Name) && !String.IsNullOrEmpty(DataType))
+            //    {
+            //        Column column = new Column { Name = Name, DataType = DataType };
+            //        columns.Add(column);
+            //        await context.Columns.AddAsync(column);
+            //    }
+            //}
+            ////await context.priceLists.AddAsync(priceList = new PriceList( priceList.Name, columns ));
+            ////context.SaveChanges();
+            //return Ok();
+            return Json(column);
+
+
         }
 
    
---
+++ Test/Domain/Repositories/ColumnsRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Test.Domain.Repositories
+{
+    public class ColumnsRepository
+    {
+        private readonly PriceContext context;
+
+        public ColumnsRepository(PriceContext context) 
+        {
+            this.context = context;
+        }
+
+    }
+}
